20326d4abb91c4a9a5760c4912aed58ee198b3fb
Angela Duran <duranangela19@gmail.com>
2018-07-01 16:12:39 -0600

    Added some more items features tests

--- spec/features/items/user_sees_an_items_index_spec.rb
+++ spec/features/items/user_sees_an_items_index_spec.rb
@@ -95,34 +95,4 @@ RSpec.describe 'a visitor' do
     end
 
   end
-
-  context 'visiting /items/id' do
-    it 'sees the individual item of a specific id' do
-      item = Item.create(name: 'Doorknob',
-                        description: 'Made of wax, weighs 50lbs',
-                        unit_price: 500,
-                        image: '/images/knob.jpg')
-
-      visit '/items/1'
-
-      expect(page).to have_content('Doorknob')
-    end
-  end
-  context 'visiting /items/new' do
-    it 'creates a new item' do
-
-      visit '/items/new'
-
-      fill_in("item[name]", with: "Beer Can")
-      fill_in("item[description]", with: "Empty")
-      fill_in("item[unit_price]", with: 20)
-      fill_in("item[image]", with: "images/rolling_rock.jpeg")
-
-      click_on("make a new dang dern item")
-
-      expect(page).to have_content("Beer Can")
-      expect(page).to have_content(20)
-      expect(page).to have_current_path('/items')
-    end
-  end
 end
