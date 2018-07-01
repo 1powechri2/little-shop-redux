fbae846e96e262b52319217efda089828b865273
Tristan M. Bambauer <tristan0bambauer@gmail.com>
2018-07-01 11:01:15 -0600

    Functioning merchant item count test and code.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -5,5 +5,11 @@ class Merchant < ActiveRecord::Base
 
   def self.total_number_of_items(merchant_id)
     merchant = Merchant.find(merchant_id)
+    merchant.items.count
+  end
+
+  def self.average_item_price_for_merchant
+    Merchant.joins(:items).select(merchants.*)
+    require 'pry';binding.pry
   end
 end
