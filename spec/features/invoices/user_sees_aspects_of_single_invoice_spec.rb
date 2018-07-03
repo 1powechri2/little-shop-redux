381be7d2b1848d4186a4ba5e6f4e2854319fddf7
Chris Powell <chris.ian.powell@gmail.com>
2018-07-03 08:16:56 -0600

    implements invoice dashboard feature tests

--- spec/features/invoices/user_sees_aspects_of_single_invoice_spec.rb
+++ spec/features/invoices/user_sees_aspects_of_single_invoice_spec.rb
@@ -1,12 +1,12 @@
 RSpec.describe Invoice do
   context 'calculations for index' do
     it 'returns the total amount for an invoice' do
-      invoice = Invoice.create(merchant_id: 12335938, status: 'pending')
+      invoice = Invoice.create(merchant_id: 1, status: 'pending')
       invoice_item_1 = InvoiceItem.create(item_id: 1, invoice_id: 1, quantity: 5, unit_price: 13635)
       invoice_item_2 = InvoiceItem.create(item_id: 2, invoice_id: 1, quantity: 9, unit_price: 23324)
       item_1 = Item.create(name: 'Cheese Whiz', description: 'Cheese that is whizzy', unit_price: 200, merchant_id: 7, image: 'http://whocares.org')
       item_2 = Item.create(name: 'Silly String', description: 'String that is silly', unit_price: 300, merchant_id: 4, image: 'http://www.idont.org')
-
+      merchant = Merchant.create(name: 'Billy')
       expected_result = ('%.2f' % (invoice.get_total_price(invoice.id).to_f/100))
 
       visit "/invoices/#{invoice.id}"
@@ -14,7 +14,7 @@ RSpec.describe Invoice do
       expect(page).to have_content(expected_result)
     end
   end
-  context 'invoice display' do 
+  context 'invoice display' do
     it 'returns all item names for an invoice' do
       invoice = Invoice.create(merchant_id: 12335938, status: 'pending')
       invoice_item_1 = InvoiceItem.create(item_id: 1, invoice_id: 1, quantity: 5, unit_price: 13635)
